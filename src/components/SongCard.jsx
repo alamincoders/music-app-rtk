b52e57c815fa97bfd293147b3134e43657c3c88b
alamincoders <developeralamin03@gmail.com>
2022-10-11 20:55:56 +0600

    Song Card Component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+/* eslint-disable quotes */
+import React from "react";
+import { useDispatch } from "react-redux";
+import PlayPause from "./PlayPause";
 
-const SongCard = () => {
+const SongCard = ({ song }) => {
   const dispatch = useDispatch();
+  const activeSong = "Text";
   return (
-    <div>
-      <Link />
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+            activeSong?.title === song.title ? "flex bg-black bg-opacity-70" : "hidden"
+          }`}
+        >
+          <PlayPause />
+        </div>
+        <img src={song?.images?.coverart} alt="song_img" />
+      </div>
     </div>
   );
 };
